3ba08d5e7017958bc1a9c66ce9d19285275f56d3
hapo-hungnx <hungnx@haposoft.com>
2020-12-28 23:21:58 +0700

    Create project

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+    <div id="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238671.18476161046!2d104.58098285984559!3d20.822123472175583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3133974072788b09%3A0xe9c4ec11b443a51b!2zTeG7mWMgQ2jDonUgRGlzdHJpY3QsIFNvbiBMYSwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1608486497444!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="true" aria-hidden="true" tabindex="0"></iframe>
+    </div>
+@endsection
